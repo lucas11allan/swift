0200da044d8bbf70f307ab36ba73ee2925ea03c1
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2021-11-16 11:07:00 -0300

    tentativa

---
+++ Eu sou rico/Eu sou rico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eu sou rico
+//
+//  Created by LUCAS ALLAN ALMEIDA OLIVEIRA on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
